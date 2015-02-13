eb15625d05f317e3f6e57698de1aa3a566f23d24
Lucas Pirola <lucaspirola@gmail.com>
2015-02-13 12:18:13 -0200

    setup requirejs to load interface example as package

--- frontend/src/javascripts/config.js
+++ frontend/src/javascripts/config.js
@@ -1,1 +1,23 @@
+// For any third party dependencies, like jQuery, place them in the lib folder.
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+    baseUrl: './static',
+    paths : {
+        riot : 'vendor/riotjs/js/riot',
+        // tags : '',
+        // ga_interface_example: 'vendor/gastos_abertos_interface_module_example/js'
+    },
+    packages: [{
+        name: "example",
+        location: "vendor/gastos_abertos_interface_module_example/js",
+    }],
+    shim : {
+        'riot': {
+            exports: 'riot'
+        }
+    }
+});
+
 
--- frontend/src/javascripts/main.js
+++ frontend/src/javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('Hello Gastos Abertos')
+// Start loading the main app file. Put all of
+// your application logic in there.
+
+require(['example', 'riot'], function (app, riot) {
+
+    window.riot = riot;
+    app.init();
+});
