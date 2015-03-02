4790ee4986404e94772e1033156641ce2090f430
Andres MRM <andresmrm@gmail.com>
2015-03-02 06:17:31 -0300

    bringing changes to frontend, static is replaced in build

--- frontend/src/javascripts/receitas/main.js
+++ frontend/src/javascripts/receitas/main.js
@@ -238,106 +238,158 @@ require(['datatables'], function (datatable) {
 
 
 
-$(function () {
+// ****************************************************
+//            BAR CHART
+// ****************************************************
+
+// Create chart
+create_bars = function(year_data, initial_level) {
+    $('#bars-container').highcharts({
+        chart: {
+            type: 'bar',
+            events: {
+                // drilldown: function(e) {
+                //     split_in_series(e);
+                // }
+            }
+        },
+        title: {
+            text: 'Receitas Prefeitura de Sao Paulo'
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+              enabled: false
+            }
+        },
+        yAxis: {
+            // min: 0,
+            title: {
+                text: 'Valores (R$)',
+                // align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            formatter: function() {
+                return '<b>' + this.series.name + '</b>: R$ ' + this.y
+            }
+                 },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: false
+                }
+            },
+            series: {
+                cursor: 'pointer',
+                events: {
+                    click: function (event) {
+                        set_series(this.options.code)
+                    }
+                }
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        series: [],
+        // series: [initial_level],
+        // drilldown: {
+        //     series: year_data
+        // }
+    });
+    bar_chart = $('#bars-container').highcharts();
+    bar_up_button = $('#bars-up-button')
+    bar_up_button.click(go_level_up)
+};
+
+// get upper level for level
+get_upper_level = function(level) {
+    // If in level '1', '2' or '9'
+    if (current_level.length == 1){
+        return 'BASE'
+    } else if (current_level != 'BASE') {
+        var levels = current_level.split('.')
+        levels.pop()
+        return levels.join('.')
+    } else {
+        return null
+    }
+}
+
+// Set displayed series in bar-chart to level
+set_series = function(level) {
+    current_level = level
+    element = year_data[level];
+    console.log("a")
+    console.log(level)
+    console.log(element)
+    bar_chart.setTitle({ text: element.name });
+
+    // points = bar_chart.series[0].points
+    num_series = bar_chart.series.length
+    for (var i = 0; i < num_series; ++i) {
+        bar_chart.series[0].remove();
+    //     console.log(bar_chart.series[0].name)
+    //     console.log(points[i])
+    //     points[i].remove()
+    }
+
+    // data = e.seriesOptions.data;
+    // (e.seriesOptions.data).forEach(function(point){
+    for (var i = element.children.length; i >= 0; --i) {
+        bar_chart.addSeries(element.children[i])
+    }
+    upper_data = year_data[get_upper_level(current_level)]
+    if (upper_data) {
+        bar_up_button.text("Subir para: " + upper_data.name)
+        bar_up_button.show()
+    } else {
+        bar_up_button.hide()
+    }
+}
+
+// Go to a upper level in bar chart
+go_level_up = function() {
+    var upper_level = get_upper_level(current_level)
+    if (upper_level) {
+        set_series(upper_level)
+    }
+}
+
+$(function() {
     var uriParams = window.location.search.substring(1);
     var query = $.deserialize(uriParams);
+
+    // Get year param
+    if (query.hasOwnProperty('year')) {
+        year = query.year;
+    } else {
+        //TODO: change this...
+        year = '2014'
+    }
 
     if (query.hasOwnProperty('level')) {
         level = query.level;
     } else {
-	get_drilldown = function (e) {
-		var chart = e.currentTarget;
-		var response_data = $.get(api_url + '/api/v1/receita/total?code=' + e.point.code + '&years=2014&drilldown=true').done(function (resp) {
-			if (resp.data) {
-				var drill_var = resp;
-				var imp_drilldown = {id: e.point.code, data:[]};
-				for (var i = 0; i < drill_var.data.length; ++i) {
-					imp_drilldown.data.push({y: drill_var.data[i].value,
-								 name: drill_var.data[i].name,
-								 code: drill_var.data[i].code,
-								 drilldown: true});
-				}
-				chart.addSeriesAsDrilldown(e.point, imp_drilldown);
-			}
-		});
-
-	}
-
-        impostos_request = $.ajax({
-                type: 'GET',
-                url: api_url + '/api/v1/receita/total?code=1.1.1&code=1.1.2&code=1.2.3&years=2014',
-                xhrFields: {
-                    withCredentials: false
-                }
-            }).done(function (response_data) {
-                impostos = {name: response_data['data'][0]['name'],
-			    data:[{y: response_data['data'][0]['value'],
-                            	   name: response_data['data'][0]['name'],
-			    	   code: response_data['data'][0]['code'],
- 			    	   drilldown: true
-                            	   }]
-			    };
-
-                taxas = {name: response_data['data'][1]['name'],
-			 data: [{y: response_data['data'][1]['value'],
-                          	 name: response_data['data'][1]['name'],
-			 	 code: response_data['data'][1]['code'],
- 			 	 drilldown: true
-                            }]
-			};
-
-                contrib = {name: response_data['data'][2]['name'],
-			    data: [{y: response_data['data'][2]['value'],
-                            name: response_data['data'][2]['name'],
-			    code: response_data['data'][2]['code'],
- 			    drilldown: true
-                            }]};
-
-                create_bars();
-            });
-
+        level = null
     }
 
-    create_bars = function () {
-        $('#bars-container').highcharts({
-            chart: {
-                type: 'bar',
-		events: {
-			drilldown: function (e) {
-				get_drilldown(e);
-			}
-		}
-
-            },
-            title: {
-                text: 'Receitas Prefeitura de Sao Paulo'
-            },
-            xAxis: {
-		type: 'category'
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'R$',
-                    align: 'high'
-                },
-                labels: {
-                    overflow: 'justify'
-                }
-            },
-            tooltip: {
-            },
-            plotOptions: {
-                bar: {
-                    dataLabels: {
-                        enabled: false
-                    }
-                }
-            },
-            credits: {
-                enabled: false
-            },
-            series: [impostos, taxas, contrib]
-        });
-    };
+    // Load ALL data for a year
+    $.ajax({
+        type: 'GET',
+        url: api_url + '/api/v1/receita/totaldrilldown?year=' + year,
+        xhrFields: {
+            withCredentials: false
+        }
+    }).done(function(response_data) {
+        year_data = response_data
+        if (level == null) level = 'BASE';
+        // initial_level.colorByPoint = true
+        create_bars();
+        set_series(level)
+    });
 });
