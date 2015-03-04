513b61e6246174ca9a09f9e923c03ac1a1c1c87f
Luiz Armesto <luiz.armesto@gmail.com>
2015-03-04 16:30:10 -0300

    using bower to install jquery and datatables

--- frontend/src/javascripts/config.js
+++ frontend/src/javascripts/config.js
@@ -5,9 +5,10 @@
 requirejs.config({
     baseUrl: '/static/javascripts',
     paths : {
+        jquery: '../vendor/jquery/js/jquery',
         riot : '../vendor/riotjs/js/riot',
         pubsub : '../vendor/pubsub-js/js/pubsub',
-        datatables: '//cdn.datatables.net/1.10.5/js/jquery.dataTables'
+        datatables: '../vendor/datatables/js/jquery.dataTables'
         // tags : 'tag',
         // ga_interface_example: 'vendor/gastos_abertos_interface_module_example/js'
     },
