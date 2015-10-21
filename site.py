043e070abbcbb93f777b02f9d1e8b2a7abee3163
Luiz Armesto <luiz.armesto@gmail.com>
2015-10-21 04:22:54 -0200

    use the host '0.0.0.0' by default
    but should be configurable in a near future

--- site.py
+++ site.py
@@ -131,4 +131,4 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'build':
         freezer.freeze()
     else:
-        app.run(port=8000)
+        app.run(host='0.0.0.0', port=8000)
